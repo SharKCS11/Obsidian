4e11623fc165cc46646679ae92e0c16135365c04
Evan <oebaoo@gmail.com>
2018-02-25 10:25:42 -0500

    update image_recognition.py

--- image_recognition.py
+++ image_recognition.py
@@ -1,11 +1,15 @@
 import cv2
 import sys
 import time
+import requests
 
 cascPath = sys.argv[1]
 faceCascade = cv2.CascadeClassifier(cascPath)
 
-video_capture = cv2.VideoCapture(1)
+WEBCAM_NUMBER = 0
+video_capture = cv2.VideoCapture(WEBCAM_NUMBER)
+
+counter = 0
 
 while True:
     # Capture frame-by-frame
@@ -32,7 +36,35 @@ while True:
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
         crop_img = frame[max_face[1]:max_face[1] + max_face[3], max_face[0]:max_face[0] + max_face[2]]
-        cv2.imshow('cropped', crop_img)
+        cv2.imwrite('temp.png', crop_img)
+        data = open('temp.png', 'rb').read()
+        name = ''
+        if counter == 5:
+            while True:
+                try:
+                    name = str(raw_input("Please enter your name: "))
+                except ValueError:
+                    print("Sorry, I didn't understand that.")
+                    continue
+                else:
+                    break
+            headers = {
+                'content-type': "application/octet-stream",
+                'name': name
+            }
+            response = requests.post(url='http://52.179.97.25:8080/new', data=data, headers=headers)
+            counter = 0
+        else:
+            headers = {
+                'content-type': "application/octet-stream"
+            }
+            response = requests.post(url='http://52.179.97.25:8080/api', data=data, headers=headers)
+
+        print(response.content)
+        if response.content == "500":
+            counter = counter + 1
+        else:
+            counter = 0
 
     cv2.imshow('video', frame)
 
@@ -40,7 +72,7 @@ while True:
     if cv2.waitKey(1) & 0xFF == 27:
         break
 
-    time.sleep(0.5)
+    time.sleep(10)
 
 video_capture.release()
 cv2.destroyAllWindows()
