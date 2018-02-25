4ebb79d4c865ea0fd061169770a2e5907605528d
sharang <sharang@umich.edu>
2018-02-25 04:55:36 -0500

    Infrastructure better

--- Protocol/meshchat.cpp
+++ Protocol/meshchat.cpp
@@ -5,27 +5,25 @@ using namespace std;
 
 /***** MESHCLIENT GLOBAL VARIABLES ******/
 vector<Node> MeshClient::neighbours;
+int MeshClient::udsock;
+struct sockaddr_in MeshClient::loc_addr;
 
 /***** MESHCLIENT STATIC FUNCTIONS ******/
 void MeshClient::node_init()
 {
-    int udsock;
-    if( !(udsock=socket(SOCKET_CONN_TYPE, SOCK_DGRAM, 0)) )
-    {
-        perror("Error creating socket.");
-        return;
-    }
-    struct sockaddr_in loc_addr;
-    memset((char *)&loc_addr, 0, sizeof(loc_addr));
-    loc_addr.sin_family = AF_INET;
-    loc_addr.sin_addr.s_addr = htonl(LOCIP);
-    loc_addr.sin_port = htons(0); //OS assigns port.
-    if(bind(udsock, (struct sockaddr *)&loc_addr, sizeof(loc_addr)) != 0)
-    {
-        perror("bind failed");
-        return;
-    }
+    setup_socket();
+    unsigned long long ms = chrono::duration_cast< chrono::milliseconds >(
+        chrono::system_clock::now().time_since_epoch()
+    ).count();
 
+    thread t_listen(MeshClient::listen);
+    t_listen.detach();
+    cout << "Thread t is listening." <<endl;
+    getUserInput();
+}
+
+void MeshClient::listen()
+{
     char rec_buffer[MSGSIZE];
     struct sockaddr_in remote_addr;
     uint32_t loclen = sizeof(loc_addr), addrlen;
@@ -37,6 +35,7 @@ void MeshClient::node_init()
     cout << "Listening on port " << ntohs(loc_addr.sin_port) <<endl;
     //receive packets.
     while(1){
+
         int recv_status = recvfrom(udsock, rec_buffer, MSGSIZE, 0, (struct sockaddr *)&remote_addr, &addrlen);
         if(recv_status < 0)
         {
@@ -44,7 +43,33 @@ void MeshClient::node_init()
             return;
         }
         printf("Received message: %s\n", rec_buffer);
+    }
+}
 
+void MeshClient::setup_socket()
+{
+    if( !(udsock=socket(SOCKET_CONN_TYPE, SOCK_DGRAM, 0)) )
+    {
+        perror("Error creating socket.");
+        return;
+    }
+    memset((char *)&loc_addr, 0, sizeof(loc_addr));
+    loc_addr.sin_family = AF_INET;
+    loc_addr.sin_addr.s_addr = htonl(BROADCAST_IP);
+    loc_addr.sin_port = htons(0); //OS assigns port.
+    if(bind(udsock, (struct sockaddr *)&loc_addr, sizeof(loc_addr)) != 0)
+    {
+        perror("bind failed");
+        return;
+    }
+
+}
+
+void MeshClient::getUserInput()
+{ //runs as the main thread.
+    string line;
+    while(getline(cin, line)){
+        cout << "Got line: " << line << endl;
     }
 }
 
--- Protocol/meshchat.h
+++ Protocol/meshchat.h
@@ -1,17 +1,19 @@
 #ifndef MESHCHAT_H //include guards
 #define MESHACHAT_H
-// Includes for Sockets
+//C includes
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+// Includes for Sockets
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 // Includes from STL
 #include <vector>
-
-
+#include <thread>
+#include <queue>
+#include <chrono>
 
 //Socket-specific definitions
 /*
@@ -24,13 +26,22 @@ struct sockaddr_in { //STRUCTURE OF A SOCKET ADDRESS
 };
 */
 #define SOCKET_CONN_TYPE AF_INET
-#define LOCIP INADDR_ANY
+#define SUBNET_MASK 0xC0A80100
 #define MSGSIZE 4096
 
+constexpr uint32_t BROADCAST_IP = SUBNET_MASK | 0xFF;
 
-typedef struct Node{
-
+//Pass nodes by value everywhere.
+typedef struct{
+    sockaddr_in address;
 } Node;
+
+typedef std::chrono::milliseconds ms_t;
+
+typedef struct{
+    unsigned long long timestamp;
+    std::string text;
+} Message;
 
 /* MeshClient will be used as a namespace for all
 global network-related functions and data */
@@ -39,8 +50,16 @@ class MeshClient
 public:
 //Class variables (globals)
     static std::vector<Node> neighbours;
+    static int udsock;
+    static sockaddr_in loc_addr;
+    static std::chrono::steady_clock CLK;
 //Class functions (globals)
     static void node_init();
+    static void node_init(Node connection);
+    static void listen();
+private:
+    static void setup_socket();
+    static void getUserInput();
 
 };
 
